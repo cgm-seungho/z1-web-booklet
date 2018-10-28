bf8a2f8246fbddfe451af7c58a31169ee64b4cc5
K.Seungho <rhanfwlq980@hanyang.ac.kr>
2018-10-29 02:54:41 +0900

    polyfills.js 오류 수정

--- z1-web/src/components/MainPage/MainPage.tsx
+++ z1-web/src/components/MainPage/MainPage.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 import * as styles from "./MainPage.scss";
+import { Button } from 'reactstrap';
 
 class MainPage extends React.Component {
     public render() {
         return (
             <div className={styles.MainPage}>
                 MainPage
+                <Button color="primary">이건 버튼입니당</Button>{' '}
             </div>
+            
         );
     }
 }
